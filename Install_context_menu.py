ce6288522940c57f9fc8177e0a103000c3b5937c
Lashika Kapoor <131615852+LashikaKapoor@users.noreply.github.com>
2026-07-28 13:42:24 -0400

    made some changes

--- Install_context_menu.py
+++ Install_context_menu.py
@@ -1,44 +1,36 @@
-#once you have this u need to open ur terminal, cs inside the state-ai...folder (the name of this one)
-#and then you need to run: python install_context_menu.py
-
-
-import shutil
 import sys
-import winreg #literly the only way to add stuff by right clicking
+import winreg
 from pathlib import Path
 
 def install():
-
     python_exe = Path(sys.executable).parent / "pythonw.exe"
     if not python_exe.exists():
         python_exe = sys.executable
 
     script_path = Path(__file__).resolve().parent / "frontend.py"
+    
 
-    command = f'"{python_exe}" "{script_path}" "%V"' #the v thing is a plceholder tbat fill in the file we clicked on
+    command = f'"{python_exe}" "{script_path}" "%V"'
 
     registry_paths = [
         r"Directory\Background\shell\OrganizeWithAI",
         r"Directory\shell\OrganizeWithAI"
     ]
 
-
     try:
         for reg_path in registry_paths:
-     
             key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, r"Software\Classes\\" + reg_path)
             winreg.SetValue(key, "", winreg.REG_SZ, "Organize Folder")
-        
+            
             sub_key = winreg.CreateKey(key, "command")
             winreg.SetValue(sub_key, "", winreg.REG_SZ, command)
-           
+            
             winreg.CloseKey(sub_key)
             winreg.CloseKey(key)
 
-        print("yay! we did it! All hail team 404 file found!")
+        print("Successfully re-registered 'Organize Folder' in Windows Context Menu!")
     except Exception as e:
-        print("trouble")
-
+        print(f"Error registering context menu: {e}")
 
 if __name__ == "__main__":
     install()
